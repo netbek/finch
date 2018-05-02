88a0ec6b22b51402fc2f632292b430594c5e8bd8
Hein Bekker <hein@netbek.co.za>
2018-05-02 23:59:56 +0200

    Remove template literals from examples to avoid confusion

--- src/docs/js/plots/histogram.js
+++ src/docs/js/plots/histogram.js
@@ -1,7 +1,7 @@
 import finch from '../../../finch/js';
 
 const {spec} = finch('data/iris.csv')
-  .title(`Edgar Anderson's Iris Data`)
+  .title("Edgar Anderson's Iris Data")
   .width(200)
   .height(200)
   .bar({binSpacing: 0})
