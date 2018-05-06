948588dd33ee60d42c6da0522f323e8e3a4985fa
Hein Bekker <hein@netbek.co.za>
2018-05-06 18:45:48 +0200

    Updates

--- vendor/@netbek/finch/umd/finch.js
+++ vendor/@netbek/finch/umd/finch.js
@@ -7,7 +7,7 @@
 		exports["finch"] = factory(require("vega"));
 	else
 		root["finch"] = factory(root["vega"]);
-})(typeof self !== 'undefined' ? self : this, function(__WEBPACK_EXTERNAL_MODULE_1__) {
+})(typeof self !== 'undefined' ? self : this, function(__WEBPACK_EXTERNAL_MODULE_2__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -79,9 +79,26 @@ return /******/ (function(modules) { // webpackBootstrap
 
 exports.__esModule = true;
 
+var _vl = __webpack_require__(1);
+
+Object.defineProperty(exports, 'vl', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_vl).default;
+  }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports.__esModule = true;
+
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _vega = __webpack_require__(1);
+var _vega = __webpack_require__(2);
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -161,9 +178,9 @@ var parseMouseoverDef = function parseMouseoverDef(mouseover) {
   };
 };
 
-var Spec = function () {
-  function Spec(data) {
-    _classCallCheck(this, Spec);
+var Vl = function () {
+  function Vl(data) {
+    _classCallCheck(this, Vl);
 
     this.spec = {
       $schema: 'https://vega.github.io/schema/vega-lite/v2.json'
@@ -171,32 +188,32 @@ var Spec = function () {
     return this.data(data);
   }
 
-  Spec.prototype.config = function config(opts) {
+  Vl.prototype.config = function config(opts) {
     this.spec.config = this.spec.config ? _extends({}, this.spec.config, opts) : opts;
     return this;
   };
 
-  Spec.prototype.title = function title(value) {
+  Vl.prototype.title = function title(value) {
     this.spec.title = value;
     return this;
   };
 
-  Spec.prototype.description = function description(value) {
+  Vl.prototype.description = function description(value) {
     this.spec.description = value;
     return this;
   };
 
-  Spec.prototype.width = function width(value) {
+  Vl.prototype.width = function width(value) {
     this.spec.width = value;
     return this;
   };
 
-  Spec.prototype.height = function height(value) {
+  Vl.prototype.height = function height(value) {
     this.spec.height = value;
     return this;
   };
 
-  Spec.prototype.data = function data(value) {
+  Vl.prototype.data = function data(value) {
     if ((0, _vega.isString)(value)) {
       this.spec.data = { url: value };
     } else if ((0, _vega.isArray)(value)) {
@@ -207,17 +224,17 @@ var Spec = function () {
     return this;
   };
 
-  Spec.prototype.transform = function transform(opts) {
+  Vl.prototype.transform = function transform(opts) {
     this.spec.transform = opts;
     return this;
   };
 
-  Spec.prototype.selection = function selection(opts) {
+  Vl.prototype.selection = function selection(opts) {
     this.spec.selection = opts;
     return this;
   };
 
-  Spec.prototype.__channel = function __channel(prop, field, type, opts) {
+  Vl.prototype.__channel = function __channel(prop, field, type, opts) {
     var maybeArray = prop === 'tooltip';
 
     if (!this.spec.encoding) {
@@ -263,144 +280,144 @@ var Spec = function () {
     return this;
   };
 
-  Spec.prototype.x = function x(field, type, opts) {
+  Vl.prototype.x = function x(field, type, opts) {
     return this.__channel('x', field, type, opts);
   };
 
-  Spec.prototype.y = function y(field, type, opts) {
+  Vl.prototype.y = function y(field, type, opts) {
     return this.__channel('y', field, type, opts);
   };
 
-  Spec.prototype.x2 = function x2(field, type, opts) {
+  Vl.prototype.x2 = function x2(field, type, opts) {
     return this.__channel('x2', field, type, opts);
   };
 
-  Spec.prototype.y2 = function y2(field, type, opts) {
+  Vl.prototype.y2 = function y2(field, type, opts) {
     return this.__channel('y2', field, type, opts);
   };
 
-  Spec.prototype.longitude = function longitude(field, type, opts) {
+  Vl.prototype.longitude = function longitude(field, type, opts) {
     return this.__channel('longitude', field, type, opts);
   };
 
-  Spec.prototype.latitude = function latitude(field, type, opts) {
+  Vl.prototype.latitude = function latitude(field, type, opts) {
     return this.__channel('latitude', field, type, opts);
   };
 
-  Spec.prototype.longitude2 = function longitude2(field, type, opts) {
+  Vl.prototype.longitude2 = function longitude2(field, type, opts) {
     return this.__channel('longitude', field, type, opts);
   };
 
-  Spec.prototype.latitude2 = function latitude2(field, type, opts) {
+  Vl.prototype.latitude2 = function latitude2(field, type, opts) {
     return this.__channel('latitude', field, type, opts);
   };
 
-  Spec.prototype.color = function color(field, type, opts) {
+  Vl.prototype.color = function color(field, type, opts) {
     return this.__channel('color', field, type, opts);
   };
 
-  Spec.prototype.opacity = function opacity(field, type, opts) {
+  Vl.prototype.opacity = function opacity(field, type, opts) {
     return this.__channel('opacity', field, type, opts);
   };
 
-  Spec.prototype.shape = function shape(field, type, opts) {
+  Vl.prototype.shape = function shape(field, type, opts) {
     return this.__channel('shape', field, type, opts);
   };
 
-  Spec.prototype.size = function size(field, type, opts) {
+  Vl.prototype.size = function size(field, type, opts) {
     return this.__channel('size', field, type, opts);
   };
 
-  Spec.prototype.label = function label(field, type, opts) {
+  Vl.prototype.label = function label(field, type, opts) {
     return this.__channel('text', field, type, opts);
   };
 
-  Spec.prototype.tooltip = function tooltip(field, type, opts) {
+  Vl.prototype.tooltip = function tooltip(field, type, opts) {
     return this.__channel('tooltip', field, type, opts);
   };
 
-  Spec.prototype.href = function href(field, type, opts) {
+  Vl.prototype.href = function href(field, type, opts) {
     return this.__channel('href', field, type, opts);
   };
 
-  Spec.prototype.detail = function detail(field, type, opts) {
+  Vl.prototype.detail = function detail(field, type, opts) {
     return this.__channel('detail', field, type, opts);
   };
 
-  Spec.prototype.key = function key(field, type, opts) {
+  Vl.prototype.key = function key(field, type, opts) {
     return this.__channel('key', field, type, opts);
   };
 
-  Spec.prototype.order = function order(field, type, opts) {
+  Vl.prototype.order = function order(field, type, opts) {
     return this.__channel('order', field, type, opts);
   };
 
-  Spec.prototype.row = function row(field, type, opts) {
+  Vl.prototype.row = function row(field, type, opts) {
     return this.__channel('row', field, type, opts);
   };
 
-  Spec.prototype.column = function column(field, type, opts) {
+  Vl.prototype.column = function column(field, type, opts) {
     return this.__channel('column', field, type, opts);
   };
 
-  Spec.prototype.__mark = function __mark(prop, opts) {
+  Vl.prototype.__mark = function __mark(prop, opts) {
     this.spec.mark = (0, _vega.isObject)(opts) ? _extends({ type: prop }, opts) : { type: prop };
     return this;
   };
 
-  Spec.prototype.area = function area(opts) {
+  Vl.prototype.area = function area(opts) {
     return this.__mark('area', opts);
   };
 
-  Spec.prototype.bar = function bar(opts) {
+  Vl.prototype.bar = function bar(opts) {
     return this.__mark('bar', opts);
   };
 
-  Spec.prototype.boxplot = function boxplot(opts) {
+  Vl.prototype.boxplot = function boxplot(opts) {
     return this.__mark('box-plot', opts);
   };
 
-  Spec.prototype.circle = function circle(opts) {
+  Vl.prototype.circle = function circle(opts) {
     return this.__mark('circle', opts);
   };
 
-  Spec.prototype.geoshape = function geoshape(opts) {
+  Vl.prototype.geoshape = function geoshape(opts) {
     return this.__mark('geoshape', opts);
   };
 
-  Spec.prototype.line = function line(opts) {
+  Vl.prototype.line = function line(opts) {
     return this.__mark('line', opts);
   };
 
-  Spec.prototype.point = function point(opts) {
+  Vl.prototype.point = function point(opts) {
     return this.__mark('point', opts);
   };
 
-  Spec.prototype.rect = function rect(opts) {
+  Vl.prototype.rect = function rect(opts) {
     return this.__mark('rect', opts);
   };
 
-  Spec.prototype.rule = function rule(opts) {
+  Vl.prototype.rule = function rule(opts) {
     return this.__mark('rule', opts);
   };
 
-  Spec.prototype.square = function square(opts) {
+  Vl.prototype.square = function square(opts) {
     return this.__mark('square', opts);
   };
 
-  Spec.prototype.text = function text(opts) {
+  Vl.prototype.text = function text(opts) {
     return this.__mark('text', opts);
   };
 
-  Spec.prototype.tick = function tick(opts) {
+  Vl.prototype.tick = function tick(opts) {
     return this.__mark('tick', opts);
   };
 
-  Spec.prototype.trail = function trail(opts) {
+  Vl.prototype.trail = function trail(opts) {
     return this.__mark('trail', opts);
   };
 
-  Spec.prototype.__concat = function __concat(prop, args) {
+  Vl.prototype.__concat = function __concat(prop, args) {
     this.spec[prop] = args.map(function (plot) {
       var _plot$spec = plot.spec,
           $schema = _plot$spec.$schema,
@@ -415,7 +432,7 @@ var Spec = function () {
     return this;
   };
 
-  Spec.prototype.layer = function layer() {
+  Vl.prototype.layer = function layer() {
     for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
       args[_key] = arguments[_key];
     }
@@ -423,7 +440,7 @@ var Spec = function () {
     return this.__concat('layer', args);
   };
 
-  Spec.prototype.hconcat = function hconcat() {
+  Vl.prototype.hconcat = function hconcat() {
     for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
       args[_key2] = arguments[_key2];
     }
@@ -431,7 +448,7 @@ var Spec = function () {
     return this.__concat('hconcat', args);
   };
 
-  Spec.prototype.vconcat = function vconcat() {
+  Vl.prototype.vconcat = function vconcat() {
     for (var _len3 = arguments.length, args = Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
       args[_key3] = arguments[_key3];
     }
@@ -439,7 +456,7 @@ var Spec = function () {
     return this.__concat('vconcat', args);
   };
 
-  Spec.prototype.__facet = function __facet(prop, opts, plot) {
+  Vl.prototype.__facet = function __facet(prop, opts, plot) {
     var _plot$spec2 = plot.spec,
         $schema = _plot$spec2.$schema,
         background = _plot$spec2.background,
@@ -453,27 +470,27 @@ var Spec = function () {
     return this;
   };
 
-  Spec.prototype.repeat = function repeat(opts, plot) {
+  Vl.prototype.repeat = function repeat(opts, plot) {
     return this.__facet('repeat', opts, plot);
   };
 
-  Spec.prototype.facet = function facet(opts, plot) {
+  Vl.prototype.facet = function facet(opts, plot) {
     return this.__facet('facet', opts, plot);
   };
 
-  return Spec;
+  return Vl;
 }();
 
 exports.default = function (data) {
-  return new Spec(data);
+  return new Vl(data);
 };
 
 /***/ }),
-/* 1 */
+/* 2 */
 /***/ (function(module, exports) {
 
-module.exports = __WEBPACK_EXTERNAL_MODULE_1__;
+module.exports = __WEBPACK_EXTERNAL_MODULE_2__;
 
 /***/ })
-/******/ ])["default"];
+/******/ ]);
 });
